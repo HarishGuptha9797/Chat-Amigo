ec81c44e30db50afdae2f6a9f6295c3148f868e6
Harish <bysaniharishguptha@gmail.com>
2024-02-29 11:49:02 +0530

    modified image path

--- src/pages/Logo.jsx
+++ src/pages/Logo.jsx
@@ -3,7 +3,7 @@ import "./Logo.css"
 const Logo = ()=>{
     return(
         <div>
-            <img src="public\images\Chat_BOX-removebg.png" alt="" className='logo' />
+            <img src="images\Chat_BOX-removebg.png" alt="" className='logo' />
         </div>
     )
 }
